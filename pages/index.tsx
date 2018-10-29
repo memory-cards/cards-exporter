041dec30e1a82d6aedc94b2391d32520257dc8d5
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-10-29 21:56:35 +0400

    Setup initial page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Cards Exporter</h1>
+      </div>
+    );
+  }
+}

ead43cfe502d7749d83174cf2e56bdf61d0d192d
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-04 19:04:03 +0400

    Add sub-router for cards

--- api/cards/index.ts
+++ api/cards/index.ts
@@ -3,6 +3,9 @@ import updateCards from "./updateCards";
 
 const router = Router();
 
+// @todo: replace with POST and validation
+// to limit user who could initiate a process
+// and avoid DOS
 router.get("/update", updateCards);
 
 export default router;

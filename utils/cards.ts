6cb7e15f90bd8e12f71849f6fb1bbbf5ff79a62d
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-06 23:11:16 +0400

    Handle windows remove command

--- utils/cards.ts
+++ utils/cards.ts
@@ -1,11 +1,14 @@
 import * as childProcess from "child_process";
 import * as util from "util";
 
+const isWin = process.platform.includes("win");
 const exec = util.promisify(childProcess.exec);
+
+const rmCommand = isWin ? "rmdir /s /q data/cards" : "rm -rf data/cards";
 
 export const setupCardsStorage = () =>
   Promise.resolve()
-    .then(() => exec("rm -rf data/cards"))
+    .then(() => exec(rmCommand))
     .then(() =>
       exec(
         "git clone https://github.com/memory-cards/cards data/cards --depth 2"

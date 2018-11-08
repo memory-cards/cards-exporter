2b3e172bb3e10124a7e0230b7773b235dfa11c5a
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-08 15:04:03 +0400

    Don't renew cards on every start

--- utils/cards.spec.ts
+++ utils/cards.spec.ts
@@ -1,7 +1,10 @@
 import "child_process";
+import "fs";
 import { setupCardsStorage, updateCardsStorage } from "./cards";
+import { rmCommand } from "./env";
 
 let execHistory: string[] = [];
+let mockCardsExists = true;
 
 jest.mock("child_process", () => ({
   exec: (command: string, cb: (_: null, result: string) => void) => {
@@ -10,19 +13,50 @@ jest.mock("child_process", () => ({
   }
 }));
 
+jest.mock("fs", () => ({
+  stat: (_: string, cb: (err: any, result: any) => void) =>
+    cb(null, mockCardsExists)
+}));
+
 describe("cardsUtils", () => {
   beforeEach(() => {
     execHistory = [];
   });
   describe("setupCardsStorage", () => {
-    it("removes old and clone new dir", async () => {
-      await setupCardsStorage();
-      expect(execHistory).toEqual(
-        expect.arrayContaining([
-          "rm -rf data/cards",
+    describe("forceRenew: true", () => {
+      it("removes old and clone new dir on force renew", async () => {
+        mockCardsExists = true;
+        await setupCardsStorage(true);
+        expect(execHistory).toEqual(
+          expect.arrayContaining([
+            rmCommand("data/cards"),
+            "git clone https://github.com/memory-cards/cards data/cards --depth 2"
+          ])
+        );
+      });
+      it("does not remove old and clone new dir on force renew", async () => {
+        mockCardsExists = false;
+        await setupCardsStorage(true);
+        expect(execHistory).toEqual(
+          expect.arrayContaining([
+            "git clone https://github.com/memory-cards/cards data/cards --depth 2"
+          ])
+        );
+      });
+    });
+    describe("forceRenew: false", () => {
+      it("does nothing on existing data/cards", async () => {
+        mockCardsExists = true;
+        await setupCardsStorage();
+        expect(execHistory).toEqual([]);
+      });
+      it("just clones repo on non-existing data/cards", async () => {
+        mockCardsExists = false;
+        await setupCardsStorage();
+        expect(execHistory).toEqual([
           "git clone https://github.com/memory-cards/cards data/cards --depth 2"
-        ])
-      );
+        ]);
+      });
     });
   });
   describe("updateCardsStorage", () => {
--- utils/cards.ts
+++ utils/cards.ts
@@ -1,23 +1,22 @@
 import * as childProcess from "child_process";
+import * as fs from "fs";
 import * as util from "util";
+import { rmCommand } from "./env";
 
-const isWin = process.platform.startsWith("win");
 const exec = util.promisify(childProcess.exec);
+const stat = util.promisify(fs.stat);
 
-// in *nix `rm` by default skip non-existing files,
-// on win* we have to use another command and check if directory exists
-const rmCommand = isWin
-  ? `if exist "data\\cards" rmdir /s /q "data\\cards"`
-  : "rm -rf data/cards";
-
-export const setupCardsStorage = () =>
-  Promise.resolve()
-    .then(() => exec(rmCommand))
-    .then(() =>
-      exec(
-        "git clone https://github.com/memory-cards/cards data/cards --depth 2"
-      )
+export const setupCardsStorage = async (forceRenew = false) => {
+  const cardsDirExists = await stat("data/cards").catch(_ => false);
+  if (forceRenew && cardsDirExists) {
+    await exec(rmCommand("data/cards"));
+  }
+  if (forceRenew || !cardsDirExists) {
+    await exec(
+      "git clone https://github.com/memory-cards/cards data/cards --depth 2"
     );
+  }
+};
 
 export const updateCardsStorage = () =>
   Promise.resolve()

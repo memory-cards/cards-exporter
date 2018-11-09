45a9802bc8eba8a8a533513e0ff4202cfe1cdc74
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-09 12:35:23 +0400

    Add env spec

--- utils/env.ts
+++ utils/env.ts
@@ -1,12 +1,18 @@
 import * as path from "path";
 
-export const isWin = process.platform.startsWith("win");
+export const getIsWindows = () => process.platform.startsWith("win");
 
 // in *nix `rm` by default skip non-existing files,
 // on win* we have to use another command and check if directory exists
-export const getRemoveCommand = (pathToRemove: string) =>
-  isWin
-    ? `if exist "${path}" rmdir /s /q "${pathToRemove
-        .split("/")
-        .join(path.sep)}"`
+export const getRemoveCommand = (pathToRemove: string) => {
+  const isWin = getIsWindows();
+
+  // technically we could use path.sep
+  // this one done for test purposes
+  const PATH_SEPARATOR = isWin ? "\\" : "/";
+
+  const winPathToRemove = pathToRemove.split("/").join(PATH_SEPARATOR);
+  return isWin
+    ? `if exist "${winPathToRemove}" rmdir /s /q "${winPathToRemove}"`
     : `rm -rf "${pathToRemove}"`;
+};

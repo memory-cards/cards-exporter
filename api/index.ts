6704838c22cdcae169c0350049289975315f507e
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-18 11:17:46 +0400

    Add Sentry intergration to express-part

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import cardsRouter from "./cards";
+import createError from "./createError";
 import dateController from "./date";
 
 const router = Router();
 
 router.get("/api/date", dateController);
 router.use("/api/cards", cardsRouter);
+router.post("/api/createError", createError);
 
 export default router;
--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/node";
 import express, { Request, Response } from "express";
 import next from "next";
 
@@ -9,14 +10,23 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+Sentry.init({
+  dsn: "https://320d89774d7344ed9c949a784a6e1c7d@sentry.io/1325279"
+});
+
 app
   .prepare()
   .then(() => cardsUtils.setupCardsStorage())
   .then(() => {
     const server = express();
+    server.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
 
+    // The error handler must be before any other error middleware
+    server.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
     server.use(api);
 
+    // The error handler must be before any other error middleware
+    server.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
     server.get("*", (request: Request, response: Response) =>
       handle(request, response)
     );

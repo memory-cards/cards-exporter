97ecdd5af9058d56213c64efe2e20dd95f14adb9
Evgeniy Dainovich <zenbysel@gmail.com>
2019-01-07 20:21:09 +0300

    fix jest coverage for all files

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,21 @@
 module.exports = {
   globals: {
     "ts-jest": {
-      tsConfig: "jest.tsconfig.json"
+      tsConfig: "jest.tsconfig.json",
+      diagnostics: {
+        warnOnly: true
+      }
     }
   },
   moduleFileExtensions: ["js", "ts", "tsx"],
   moduleNameMapper: {
     "^~/(.*)": "<rootDir>/$1"
   },
+  collectCoverageFrom: [
+    "<rootDir>/**/*.{ts,tsx}",
+    "!**/node_modules/**",
+    "!**/typings/**"
+  ],
   setupTestFrameworkScriptFile: "<rootDir>/enzyme.js",
   silent: false,
   testEnvironment: "node",

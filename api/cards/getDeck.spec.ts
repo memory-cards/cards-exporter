432ae5ac7a78a145cfb5647746bac0489ad50ba2
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-12 17:33:46 +0400

    Add tests for `getDeck`

---
+++ api/cards/getDeck.spec.ts
@@ -0,0 +1,65 @@
+import * as httpMocks from "node-mocks-http";
+import * as cards from "~/utils/cards";
+import * as decks from "~/utils/decks";
+import getDeckController from "./getDeck";
+
+describe("getDeckController", () => {
+  let req: httpMocks.MockRequest<any>;
+  let res: httpMocks.MockResponse<any>;
+  let mockFilterKnownCards: jest.SpyInstance<any>;
+  let mockGetAllCards: jest.SpyInstance<any>;
+  let generateDeckSpy: jest.SpyInstance<any>;
+  let sendFileSpy: jest.SpyInstance<any>;
+  let sendSpy: jest.SpyInstance<any>;
+
+  beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    sendFileSpy = res.sendFile = jest.fn();
+    sendSpy = jest.spyOn(res, "send");
+    mockFilterKnownCards = jest.spyOn(cards, "filterKnownCards");
+    mockGetAllCards = jest.spyOn(cards, "getAllCards");
+    generateDeckSpy = jest.spyOn(decks, "generateDeck");
+  });
+  afterEach(() => {
+    generateDeckSpy.mockRestore();
+    mockGetAllCards.mockRestore();
+    mockFilterKnownCards.mockRestore();
+  });
+  it("calls pipe of functions and send file with result", async () => {
+    const getAllCardsMock: any[] = [];
+    const filterKnownCardsMock: any[] = [];
+    mockGetAllCards.mockReturnValue(Promise.resolve(getAllCardsMock));
+    mockFilterKnownCards.mockReturnValue(Promise.resolve(filterKnownCardsMock));
+
+    await getDeckController(req, res);
+    expect(mockGetAllCards).toHaveBeenCalled();
+    expect(mockFilterKnownCards).toHaveBeenCalledWith(getAllCardsMock);
+    expect(generateDeckSpy).toHaveBeenCalledWith(filterKnownCardsMock);
+    expect(sendFileSpy).toHaveBeenCalled();
+  });
+
+  it("sends error response of any fail 1", async () => {
+    mockGetAllCards.mockImplementation(() =>
+      Promise.reject(new Error("some strange error"))
+    );
+    await getDeckController(req, res);
+    expect(res.send).toHaveBeenCalled();
+    expect(res.send.mock.calls[0][0]).toMatchObject({
+      apiName: "cards/getDeck",
+      message: "some strange error"
+    });
+  });
+
+  it("sends error response of any fail 2", async () => {
+    mockFilterKnownCards.mockImplementation(() => {
+      throw new Error("some strange error 2");
+    });
+    await getDeckController(req, res);
+    expect(res.send).toHaveBeenCalled();
+    expect(res.send.mock.calls[0][0]).toMatchObject({
+      apiName: "cards/getDeck",
+      message: "some strange error 2"
+    });
+  });
+});

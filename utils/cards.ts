50bde3f80c14c59ba743edbace1c2e478ca82295
Evgeniy Dainovich <zenbysel@gmail.com>
2019-03-07 17:24:16 +0300

    remove testing function

--- utils/cards.ts
+++ utils/cards.ts
@@ -21,7 +21,6 @@ export const setupCardsStorage = async (isForceRenew = false) => {
       "git clone https://github.com/memory-cards/cards data/cards --depth 2"
     );
   }
-  testFunction();
 };
 
 export const updateCardsStorage = () =>
@@ -70,9 +69,3 @@ export const getAllCards = (): Promise<ICardDefinition[]> =>
 
 export const filterKnownCards = (list: ICardDefinition[]): ICardDefinition[] =>
   list.filter(el => isCardTypeExists(el.type));
-
-const testFunction = () => {
-  const a = 15;
-  const b = 25;
-  const c = a + b;
-};

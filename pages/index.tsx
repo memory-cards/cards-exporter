52d9cc40a39f3545ed7dd7896875905e2aa0c2ec
Evgeniy Dainovich <zenbysel@gmail.com>
2019-10-01 16:05:38 +0300

    add simple menu with links at main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,28 @@ import * as React from "react";
 
 export default class Index extends React.Component {
   public render() {
-    return <h1>Cards Exporter</h1>;
+    return (
+      <div>
+        <h1>Cards Exporter</h1>
+        <h3>Pages:</h3>
+        <ul>
+          <li>
+            <a href="/api/date">Date</a>
+          </li>
+          <li>
+            <a href="/api/cards/list">Cards list</a>
+          </li>
+          <li>
+            <a href="/api/cards/deck">Get deck</a>
+          </li>
+          <li>
+            <a href="/api/cards/update">Update</a>
+          </li>
+          <li>
+            <a href="/api/cards/tags">Tags</a>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }

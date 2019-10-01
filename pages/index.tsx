dfdea227cd0fc34b17b36e8bcc9f064355b3df91
Evgeniy Dainovich <zenbysel@gmail.com>
2019-10-01 17:42:48 +0300

    add links

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,25 @@ export default class Index extends React.Component {
         <h3>Pages:</h3>
         <ul>
           <li>
-            <a href="/api/date">Date</a>
+            <a href="/api/date">Date info</a>
           </li>
           <li>
-            <a href="/api/cards/list">Cards list</a>
+            <a href="/api/cards/list">Cards info</a>
           </li>
           <li>
-            <a href="/api/cards/deck">Get deck</a>
+            <a href="/api/cards/deck">Deck info</a>
           </li>
           <li>
-            <a href="/api/cards/update">Update</a>
+            <a href="/api/cards/update">Last update info</a>
           </li>
           <li>
-            <a href="/api/cards/tags">Tags</a>
+            <a href="/api/cards/tags">Tags info</a>
+          </li>
+          <li>
+            <a href="/filterCards">Filter cards page</a>
+          </li>
+          <li>
+            <a href="/updateCards">Update cards page</a>
           </li>
         </ul>
       </div>

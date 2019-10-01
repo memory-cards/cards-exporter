779d3ee351fc4c044eedb798cd3baefb7ed76dd9
zenby <32563455+zenby@users.noreply.github.com>
2019-10-01 17:45:50 +0300

    Add simple menu with links at main page (#27)
    * add simple menu with links at main page
    
    * update failing tests
    
    * update links
    
    * add links

--- pages/index.spec.tsx
+++ pages/index.spec.tsx
@@ -1,13 +1,50 @@
-import { shallow } from "enzyme";
+import { shallow, ShallowWrapper } from "enzyme";
 import * as React from "react";
 import * as TestRenderer from "react-test-renderer";
 
 import Index from "./index";
 
 describe("With Enzyme", () => {
-  it('IndexPage shows "Cards Exporter"', () => {
-    const page = shallow(<Index />);
-    expect(page.find("h1").text()).toEqual("Cards Exporter");
+  let page: ShallowWrapper;
+
+  beforeEach(() => {
+    page = shallow(<Index />);
+  });
+
+  describe("Main page", () => {
+    it('has title "Cards Exporter"', () => {
+      expect(page.find("h1").text()).toEqual("Cards Exporter");
+    });
+
+    describe("has menu with link to", () => {
+      it("date info", () => {
+        expect(page.find('a[href="/api/date"]').exists()).toBe(true);
+      });
+
+      it("cards list info", () => {
+        expect(page.find('a[href="/api/cards/list"]').exists()).toBe(true);
+      });
+
+      it("deck info", () => {
+        expect(page.find('a[href="/api/cards/deck"]').exists()).toBe(true);
+      });
+
+      it("update card info", () => {
+        expect(page.find('a[href="/api/cards/update"]').exists()).toBe(true);
+      });
+
+      it("tags info", () => {
+        expect(page.find('a[href="/api/cards/tags"]').exists()).toBe(true);
+      });
+
+      it("filter cards page", () => {
+        expect(page.find('a[href="/filterCards"]').exists()).toBe(true);
+      });
+
+      it("update cards page", () => {
+        expect(page.find('a[href="/updateCards"]').exists()).toBe(true);
+      });
+    });
   });
 });
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,34 @@ import * as React from "react";
 
 export default class Index extends React.Component {
   public render() {
-    return <h1>Cards Exporter</h1>;
+    return (
+      <div>
+        <h1>Cards Exporter</h1>
+        <h3>Pages:</h3>
+        <ul>
+          <li>
+            <a href="/api/date">Date info</a>
+          </li>
+          <li>
+            <a href="/api/cards/list">Cards info</a>
+          </li>
+          <li>
+            <a href="/api/cards/deck">Deck info</a>
+          </li>
+          <li>
+            <a href="/api/cards/update">Last update info</a>
+          </li>
+          <li>
+            <a href="/api/cards/tags">Tags info</a>
+          </li>
+          <li>
+            <a href="/filterCards">Filter cards page</a>
+          </li>
+          <li>
+            <a href="/updateCards">Update cards page</a>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }

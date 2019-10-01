2d9d8777326e44b48f22c203ee990c9f64360c16
Evgeniy Dainovich <zenbysel@gmail.com>
2019-10-01 16:53:20 +0300

    update failing tests

--- pages/index.spec.tsx
+++ pages/index.spec.tsx
@@ -1,13 +1,42 @@
-import { shallow } from "enzyme";
+import { shallow, ShallowWrapper } from "enzyme";
 import * as React from "react";
 import * as TestRenderer from "react-test-renderer";
 
 import Index from "./index";
 
 describe("With Enzyme", () => {
-  it('IndexPage shows "Cards Exporter"', () => {
-    const page = shallow(<Index />);
-    expect(page.find("h1").text()).toEqual("Cards Exporter");
+  let page: ShallowWrapper;
+
+  beforeEach(() => {
+    page = shallow(<Index />);
+  });
+
+  describe("Main page", () => {
+    it('has title "Cards Exporter"', () => {
+      expect(page.find("h1").text()).toEqual("Cards Exporter");
+    });
+
+    describe("has menu with link to", () => {
+      it("date page", () => {
+        expect(page.find('a[href="/api/date"]').exists()).toBe(true);
+      });
+
+      it("cards list page", () => {
+        expect(page.find('a[href="/api/cards/list"]').exists()).toBe(true);
+      });
+
+      it("deck page", () => {
+        expect(page.find('a[href="/api/cards/deck"]').exists()).toBe(true);
+      });
+
+      it("update card page", () => {
+        expect(page.find('a[href="/api/cards/update"]').exists()).toBe(true);
+      });
+
+      it("tags page", () => {
+        expect(page.find('a[href="/api/cards/tags"]').exists()).toBe(true);
+      });
+    });
   });
 });
 

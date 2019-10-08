76d2f07725df85eb2eb180dc1072eb67044706b9
Evgeniy Dainovich <zenbysel@gmail.com>
2019-10-08 17:03:55 +0300

    remove package.json from list cards

--- api/cards/listCards.ts
+++ api/cards/listCards.ts
@@ -1,22 +1,10 @@
 import { Request, Response } from "express";
-import * as fs from "fs";
-import * as glob from "glob";
-import * as json5 from "json5";
-import * as util from "util";
-
-const globPromised = util.promisify(glob.Glob);
-const readFilePromised = util.promisify(fs.readFile);
+import { getAllCards } from "~/utils/cards";
 
 export default (_: Request, response: Response) =>
-  globPromised("data/cards/**/*.json*").then((files: string[]) =>
-    Promise.all(
-      files.map(fileName =>
-        readFilePromised(fileName).then(item => json5.parse(item.toString()))
-      )
-    ).then(filesInfo =>
-      response.send({
-        apiName: "cards/list",
-        cards: filesInfo
-      })
-    )
+  getAllCards().then(filesInfo =>
+    response.send({
+      apiName: "cards/list",
+      cards: filesInfo
+    })
   );
